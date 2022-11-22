2b3115aade3031d64dae44b87a7cf5605b623419
8917KG <8917@ait.nsw.edu.au>
2022-11-22 13:26:07 +1100

    normal changes

--- src/App.js
+++ src/App.js
@@ -11,134 +11,132 @@ import { Signout } from './pages/Signout'
 import { Signin } from './pages/Signin'
 
 // import firebase 
-import { initializeApp } from  "firebase/app";
+import { initializeApp } from "firebase/app";
 import { FirebaseConfig } from './config/FirebaseConfig';
-import { getFirestore, getDocs, collection } from "firebase/firestore";   
+import { getFirestore, getDocs, collection } from "firebase/firestore";
 
 //import firebase auth
-import {getAuth, 
-  createUserWithEmailAndPassword, 
-  onAuthStateChanged, 
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
   signOut,
-  signInWithEmailAndPassword } 
-  from "firebase/auth"
+  signInWithEmailAndPassword
+}from "firebase/auth"
 
-import {getStorage, ref, getDownloadURL} from "firebase/storage"
+import { getStorage, ref, getDownloadURL } from "firebase/storage"
 
 //initialise Firebase 
 const FBapp = initializeApp(FirebaseConfig);
 //initialise Firebase auth 
-const FBauth = getAuth(FBapp) 
+const FBauth = getAuth(FBapp)
 
 //initialise Firestore Database
 const FBdb = getFirestore(FBapp)
 
 //initialise Firebase Storage
-const FBstorage = getStorage() 
+const FBstorage = getStorage()
 
 
 
 //function to create user account
 const signup = (email, password) => {
-  return new Promise (( resolve, reject )=> {
-    createUserWithEmailAndPassword(FBauth,email, password)
-    .then((userCredential) => resolve(userCredential.user))
-    .catch((error) => reject(error))
+  return new Promise((resolve, reject) => {
+    createUserWithEmailAndPassword(FBauth, email, password)
+      .then((userCredential) => resolve(userCredential.user))
+      .catch((error) => reject(error))
   })
 }
 
-const signin =(email, password) => {
-  return new Promise ((resolve, reject) => {
+const signin = (email, password) => {
+  return new Promise((resolve, reject) => {
     signInWithEmailAndPassword(FBauth, email, password)
-    .then((userCredential) => resolve(userCredential.user))
+      .then((userCredential) => resolve(userCredential.user))
   })
 }
 
 const signOutUser = () => {
   return new Promise((resolve, reject) => {
     signOut(FBauth)
-    .then (() =>resolve(true) )
-    .catch ((error) => reject(error))
+      .then(() => resolve(true))
+      .catch((error) => reject(error))
   })
-  
+
 }
 
 const NavData = [
-  {name: "Home", path: "/", public: true},
-  {name: "About", path: "/about", public: true},
-  {name: "Contact", path: "/contact", public: true},
-  {name: "Sign Up", path: "/signup", public: true},
-  {name: "Sign In", path: "/signin", public: true},
+  { name: "Home", path: "/", public: true },
+  { name: "About", path: "/about", public: true },
+  { name: "Contact", path: "/contact", public: true },
+  { name: "Sign Up", path: "/signup", public: true },
+  { name: "Sign In", path: "/signin", public: true },
 ]
 
 const NavDataAuth = [
-  {name: "Home", path: "/", public: true},
-  {name: "About", path: "/about", public: true},
-  {name: "Contact", path: "/contact", public: true},
-  {name: "Sign Out", path: "/signout", public: true} 
+  { name: "Home", path: "/", public: true },
+  { name: "About", path: "/about", public: true },
+  { name: "Contact", path: "/contact", public: true },
+  { name: "Sign Out", path: "/signout", public: true }
 ]
 
 function App() {
 
-  const [auth , setAuth] = useState()
-  const [nav, setNav] = useState(NavData) 
+  const [auth, setAuth] = useState()
+  const [nav, setNav] = useState(NavData)
   const [data, setData] = useState([])
 
-  useEffect(()=>{
-    if(data.length === 0){
+  useEffect(() => {
+    if (data.length === 0) {
       setData(getDataCollection('keyboards'))
     }
-  },[data])
+  }, [data])
 
-onAuthStateChanged( FBauth, (user) => {
-  if (user){
-    // console.log(user)
-    setAuth(user)
-    setNav(NavDataAuth)
-  }
-  else{
-    // console.log("not signed in.")
-    setAuth(null)
-    setNav(NavData)
-  }
-})
+  onAuthStateChanged(FBauth, (user) => {
+    if (user) {
+      // console.log(user)
+      setAuth(user)
+      setNav(NavDataAuth)
+    }
+    else {
+      // console.log("not signed in.")
+      setAuth(null)
+      setNav(NavData)
+    }
+  })
 
   const getDataCollection = async (path) => {
-    const collectionData = await getDocs( collection (FBdb, path))
+    const collectionData = await getDocs(collection(FBdb, path))
     let dbItems = []
-    collectionData.forEach((doc)=>{
+    collectionData.forEach((doc) => {
       let item = doc.data
       item.id = doc.id
       dbItems.push(item)
-    }) 
+    })
     setData(dbItems)
     //return dbItems
-  } 
+  }
 
   const getImageURL = (path) => {
     const ImageRef = ref(FBstorage, path)
-    return new Promise ((resolve, reject) => {
-      getDownloadURL (ImageRef)
-      .then((url) => resolve(url))
-      .catch((error) => reject(error))
-      })
+    return new Promise((resolve, reject) => {
+      getDownloadURL(ImageRef)
+        .then((url) => resolve(url))
+        .catch((error) => reject(error))
+    })
   }
   
-
-
-
   return (
     <div className="App">
-      <Header title = "Keyboard World"  headernav = {nav}/>
+      <Header title="Keyboard World" headernav={nav} />
       <Routes>
-        <Route path ="/" element = {<Home listData = {data} imageGetter = {getImageURL}/>} />
-        <Route path ="/about" element = {<About/>} />
-        <Route path ="/contact" element = {<Contact/>} />
-        <Route path='/signup' element = {<Signup handler = {signup}/>}/>
-        <Route path='/signout' element = {<Signout handler={signOutUser} auth = {auth}/>}/>
-        <Route path='/signin' element = {<Signin handler = {signin}/>}/>
+        <Route path="/" element={<Home listData={data} imageGetter={getImageURL} />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path='/signup' element={<Signup handler={signup} />} />
+        <Route path='/signout' element={<Signout handler={signOutUser} auth={auth} />} />
+        <Route path='/signin' element={<Signin handler={signin} />} />
       </Routes>
-      <Footer year = "2022" /> 
+      <Footer year="2022" />
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,54 +1,54 @@
 import { useState, useEffect } from "react"
-export function Home (props){
+export function Home(props) {
     const [pageData, setPageData] = useState([])
     useEffect(() => {
         setPageData(props.listData)
     })
 
-    if (pageData.length > 0 ){
-        const itemCollection = pageData.map((item, key)=>{
-        return (
-            <div className="col-md-4" key = {key}>
-                <div className="card">
-                <Image urlgetter = {props.imageGetter} imgPath = {"keyboard_photos/" + item.KeebPhoto}/>
-                    <div className="card-body">
-                        <h5 className="card-title">{item.KeebName}</h5>
+    if (pageData.length > 0) {
+        const itemCollection = pageData.map((item, key) => {
+            return (
+                <div className="col-md-4" key={key}>
+                    <div className="card">
+                        <Image urlgetter={props.imageGetter} imgPath={"keyboard_photos/" + item.KeebPhoto} />
+                        <div className="card-body">
+                            <h5 className="card-title">{item.KeebName}</h5>
+                        </div>
                     </div>
                 </div>
-            </div>
-        )
-    })
-        return(   
+            )
+        })
+        return (
             <div className="container my-4">
                 <div className="row">
                     {itemCollection}
                 </div>
-            </div>      
+            </div>
         )
-        }
+    }
     else {
-        return(
-            <div className = "container"></div>
+        return (
+            <div className="container"></div>
         )
     }
 }
 
-function Image(props){
+function Image(props) {
     const [imageURL, setImageURL] = useState()
-    useEffect (() => {
-        if(!imageURL){
+    useEffect(() => {
+        if (!imageURL) {
             props.urlgetter(props.imgPath)
-            .then((url) => setImageURL(url))
-            .catch((error) => console.log(error))
-        } 
+                .then((url) => setImageURL(url))
+                .catch((error) => console.log(error))
+        }
     })
-    if (imageURL){
-        return( 
-            <img src = {imageURL} className ="card-img-top" alt={props.KeebName}/>
+    if (imageURL) {
+        return (
+            <img src={imageURL} className="card-img-top" alt={props.KeebName} />
         )
     }
-    else{
-        return(
+    else {
+        return (
             <div>Loading...</div>
         )
     }
