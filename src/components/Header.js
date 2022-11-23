af25fb1a55631a0eb0dd13a502028018d97f7483
8917KG <8917@ait.nsw.edu.au>
2022-11-23 18:35:34 +1100

    upper nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,29 @@
 import { Navigation } from "./Navigation"
-import { Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 export function Header(props) {
-    return(
-        <header className='navbar bg-light navbar-expand-lg ' >
-            <div className='container-fluid ' >
-                <Link className='navbar-brand' to='/'>{props.title}</Link>
-                <div className="row align-item-end">
-                    <div className="collapse navbar-collapse">
-                        <Navigation items = {props.headernav}/>
+    return (
+
+        <header>
+            <div className="navbar navbar-expand-lg  bg-dark">
+                <div className="container-fluid align-item-end">
+                    <div className="">
+                        <Link className='navbar-brand text-white' to='/signin'>Sign in</Link>
                     </div>
                 </div>
             </div>
-        </header> 
+            <header className='navbar p-3 navbar-expand-lg' >
+                <div className='container-fluid' >
+                    <Link className='navbar-brand' to='/'>{props.title}</Link>
+                    <div className="row align-item-end ">
+                        <div className="collapse navbar-collapse ">
+                            <Navigation items={props.headernav} />
+                        </div>
+                    </div>
+                </div>
+            </header>
+        </header>
+
+
+
     )
 }  
