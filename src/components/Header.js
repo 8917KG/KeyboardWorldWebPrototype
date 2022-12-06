a46353c59d6ac0056529b3c46c6f5162e673e1b3
8917KG <8917@ait.nsw.edu.au>
2022-12-06 17:49:35 +1100

    re-arranged the nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,35 +3,27 @@ import { Link } from "react-router-dom"
 export function Header(props) {
     return (
 
-
-        <>
-            <nav class="navbar navbar-expand-lg bg-dark ">
-                <div class="container-fluid ">
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-
-                        <div class="nav navbar-nav ms-auto">
-                            <Link className='nav-item text-white text-decoration-none' to='/signin'>Sign in</Link>
-                        </div>
-
+        <header>
+            <nav className="navbar bg-dark navbar-expand-lg">
+                <div className="container-fluid">
+                    <div className="nav navbar-nav ms-auto">
+                        <Link className='nav-item text-white text-decoration-none' to='/signin'>Sign in</Link>
                     </div>
                 </div>
-            </nav >
-            <header className='navbar navbar-expand-lg' >
-                <div className='container-fluid' >
-                    <Link className='navbar-brand' to='/'>{props.title}</Link>
+
+            </nav>
+            <nav className="navbar bg-light navbar-expand-lg">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" to="/">{props.title}</Link>
                     <div className="row align-item-end ">
-                        <div className="collapse navbar-collapse ">
+                        <div className="collapse navbar-collapse">
                             <Navigation items={props.headernav} />
                         </div>
                     </div>
                 </div>
-            </header>
-
-        </>
-
+            </nav>
+        </header>
 
     )
-} 
+}
+
